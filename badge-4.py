dd9b53b2095c950a80df18be53cfd96f4a43fea4
zachahrens33 <107057426+zachahrens33@users.noreply.github.com>
2025-05-13 15:07:56 -0500

    Update badge-4.py

--- badge-4.py
+++ badge-4.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+import requests
 from snowflake.snowpark.functions import col
 import pandas as pd
 
 st.title("Zena's Amazing Athleisure Catalog")
 
-session = get_active_session()
+session = cnx.session()
 
 # get a list of colors for a drop list selection
 table_colors = session.sql("select color_or_style from catalog_for_website")
